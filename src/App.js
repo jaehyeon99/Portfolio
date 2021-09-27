0df6f107f76a121d8c4aa72ba875698cf62ade86
Jae_Hyeon Park <janhyun99@naver.com>
2021-09-27 18:47:07 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,19 +6,18 @@ import Head from './components/Head';
 import Header from './components/Header';
 import Project from './components/Project';
 import Skils from './components/Skils';
-import { Route, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+   
         <Header />
         <Head />
-        <Route path="/AboutMe" component={AboutMe} />
-        <Route path="/Skils" component={Skils} />
-        <Route path="/Project" component={Project} />
-        <Route path="/Contact" component={Contact} />
-      </HashRouter>
+        <AboutMe/>
+        <Skils/>
+        <Project/>
+        <Contact/>
+     
     </div>
   );
 }
